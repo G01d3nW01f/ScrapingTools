1c6f8ef86f861bdc4b070a474aa3d14dfe8646f5
Ki11i0n4ir3 <75846902+Ki11i0n4ir3@users.noreply.github.com>
2021-11-15 16:09:42 +0900

    Update catacumba.py

--- catacumba.py
+++ catacumba.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 import os.path
-from bs4 import BeautifulSoup as bs4
 from os import makedirs
 import re
 import sys
 import subprocess
-
+from bs4 import BeautifulSoup as bs4
 
 class bcolors:
 
@@ -16,7 +15,7 @@ class bcolors:
     YELLOW = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-
+    WHITE = '\033[37m'
 
 banner = """
  _______ _______ _______ _______ _______ _     _ _______ ______  _______
@@ -50,19 +49,19 @@ banner = """
 
 def init():
     
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print(bcolors.RED,"[!]Need More Argument!!!!!!!!!!!!!",bcolors.ENDC)
         usage = """
                         [!]Usage:
           \       /   
-            .---.       {arg0} <TargetDirectory>
+            .---.       {arg0} <TargetDirectory> <KeyWordList>
        '-.  |   |  .-'    
          ___|   |___    [+]First.... capture the dir and files with gdigger.py to target_site
         [           ]   [+]Step2.... this script run aim to captured 
-        `---.   .---'
-     __||__ |   | __||__
+        `---.   .---'   
+     __||__ |   | __||__[!]Example:
      '-..-' |   | '-..-'
-       ||   |   |   ||
+       ||   |   |   ||  {arg0} target.directory cmsdefault.txt
        ||_.-|   |-,_||
      .-"`   `"`'`   `"-.
    .'                   '.
@@ -72,25 +71,174 @@ def init():
         sys.exit()
 
     target_dir = sys.argv[1]
+    keyword_list = sys.argv[2]
     
     try:
-        os.chdir(target_dir)
-        print(bcolors.BLUE,banner,bcolors.ENDC)
+
+        f = open(keyword_list,"r")
+        keyword_list = f.read().split('\n')
+        
     except:
-        print(bcolors.RED,"[!]SomeException Occured!!!!",bcolors.ENDC)
+
+        print(bcolors,"[!]Some Exception Occured!!!!",bcolors.ENDC)
         sys.exit()
 
-def get_list():
+    while True:
+        keyword_list.remove("")
+            
+        if "" not in keyword_list:
+            break
     
-    all_list = subprocess.getouput("ll")
-    print(all_list)
+    list_attack_flag = subprocess.getoutput(f"file {target_dir}")
+    
+    absolute_path = os.getcwd()
+    #os.chdir(target_dir)
+    print(bcolors.BLUE,banner,bcolors.ENDC)
+    
+    if "directory" not in list_attack_flag:
+        print(bcolors.RED,"[*]List_Attack",bcolors.ENDC)
+        signal = "List"
+
+    else:
+        print(bcolors.RED,"[*]Single_Attack",bcolors.ENDC)
+        signal = "Single"
+        os.chdir(target_dir)
+
+    return absolute_path,keyword_list,signal
+    
+   
+def get_list(absolute_path):
+    
+    current_directory = os.getcwd()
+
+    all_dir = subprocess.getoutput(f"find {current_directory} -type d 2> /dev/null")
+    all_dir = all_dir.rsplit("\n")
+
+    #print(bcolors.GREEN,"[+]All_Directory....",bcolors.ENDC)
+    
+    for i in all_dir:
+        i = i.replace(absolute_path,"")
+        #print(bcolors.BLUE,i,bcolors.ENDC)
+    
+    del all_dir,i 
+    return current_directory
+
+
+def file_path(current_directory,absolute_path):
+
+    all_file = subprocess.getoutput(f"find {current_directory} -type f 2> /dev/null")
+    all_file = all_file.rsplit("\n")
+
+    print(bcolors.GREEN,"[+]founded_file:",bcolors.ENDC)
+    
+    for i in all_file:
+        i = i.replace(absolute_path,"")
+        print(bcolors.BLUE,i,bcolors.ENDC)
+
+    return all_file
+
+def counter(all_file,keyword_list):
+    
+    dic = {}
+    print(bcolors.GREEN,"[+]KeyWordList: ",bcolors.ENDC)
+    print(bcolors.YELLOW,keyword_list,bcolors.ENDC)
+    for i in keyword_list:
+        dic[i] = 0
+    
+    #print(dic)
+
+    for i in all_file:
+        
+        f = open(i,"r")
+
+        try:
+            soup = bs4(f,"html.parser")
+            #print(soup.text)
+            
+            for i in keyword_list:
+                
+                pointer = soup.text.count(i)
+                dic[i]+=pointer
+    
+        except:
+            pass
+    
+    for i in keyword_list:
+        
+        if dic[i] == 0:
+            print(bcolors.RED,f"{i} --> {dic[i]}",bcolors.ENDC)
+        
+        elif dic[i] > 9:
+            print(bcolors.WHITE,f"{i} --> {dic[i]}",bcolors.ENDC)
+
+        else:
+            print(bcolors.BLUE,f"{i} --> {dic[i]}",bcolors.ENDC)
+        
+    return keyword_list,dic
+
+def report_manager(absolute_path,current_directory):
+
+    file_dir = current_directory.replace(absolute_path,"")
+    file_dir = file_dir.replace("/","report.")
+    
+    print(bcolors.YELLOW,f"Write to: {file_dir}",bcolors.ENDC)
+    print(bcolors.YELLOW,f"FullPath: {current_directory}"+"/"+f"{file_dir}",bcolors.ENDC)
+
+    return file_dir
+
+def processing(keyword_list,dic,file_dir):
+
+    f = open(file_dir,"w")
+    f.write(file_dir.replace("report.","")+"\n")
+    for i in keyword_list:
+
+        f.write(i+","+str(dic[i])+"\n")
+
+    f.close()
+    #os.system(f"cat {file_dir}")
+
+def list_cutter(keyword_list):
+    
+    target_list = sys.argv[1]
+    f = open(target_list,"r")
+    target_list = f.read().split("\n")
+    while True:
+        target_list.remove("")
+        if "" not in target_list:
+            break
+    current_dir = os.getcwd()
+    for i in target_list:
+
+        specify_dir_name = subprocess.getoutput(f"find {current_dir} -type d -name {i} 2>/dev/null")
+        print(specify_dir_name)
+    
+
+    
 
 
 
 def main():
 
-    init()
-    get_list()
+    absolute_path,keyword_list,signal = init()
+    
+    if signal == "Single":
+
+        current_directory = get_list(absolute_path)
+        all_file = file_path(current_directory,absolute_path)
+        keyword_list,dic = counter(all_file,keyword_list)
+        file_dir = report_manager(absolute_path,current_directory)
+        processing(keyword_list,dic,file_dir)
+
+    else:
+
+        list_cutter(keyword_list)
+        
+        
+
+#######################################################
+# MainFunction is not write for some implementation   #
+# why? beacuse I can not management god damn function #  
+#######################################################
 
 if __name__ == "__main__":
 
